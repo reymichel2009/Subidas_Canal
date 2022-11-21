2b6417862d9aaba627ca109e969a693416e0e2bf
labestia2080 <100966021+reymichel2009@users.noreply.github.com>
2022-11-21 16:16:14 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.environ.get('api_hash','d1a5974070430293b64fde5339591447')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','reymichel2009').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFDDKDYEJIJFGFYDJEGKGJYEIJIIRDGELGGJKG'))
+ProxyCloud.parse(os.environ.get('proxy_enc','socks5h://KIDHKDYDJHJHGIYJJHGGGDYJIDIDRKGILKGGKG'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
